b4672ffd3f201bf0e8a3c8bf98fc9c6c9ea490d2
tarun-kataruka <tarunkataruka22@gmail.com>
2025-10-02 20:38:29 +0530

    fix: simplify API URL fetching in login and signup components

--- client/app/(auth)/signup.tsx
+++ client/app/(auth)/signup.tsx
@@ -34,9 +34,7 @@ export default function SignupScreen() {
 
     try {
       const res = await fetch(
-        process.env.EXPO_PUBLIC_API_URL
-          ? `${process.env.NEXT_PUBLIC_API_URL}api/auth/signup`
-          : 'http://localhost:5000/api/auth/signup',
+      `${process.env.NEXT_PUBLIC_API_URL}api/auth/signup`,
         {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
