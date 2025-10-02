01be5ef8b621c61b7814f31b7ea4afe0f9b110ec
tarun-kataruka <tarunkataruka22@gmail.com>
2025-10-02 20:46:10 +0530

    fix: update API URL to use EXPO_PUBLIC environment variable in auth components

--- client/app/(auth)/signup.tsx
+++ client/app/(auth)/signup.tsx
@@ -34,7 +34,7 @@ export default function SignupScreen() {
 
     try {
       const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}api/auth/signup`,
+      `${process.env.EXPO_PUBLIC_API_URL}api/auth/signup`,
         {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
